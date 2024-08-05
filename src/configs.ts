22e5be499719497f00a26d50925194f3695f2100
llOrtegall <IvanOrtega_97@hotmail.com>
2024-08-05 11:08:41 -0500

    cambios a la api

--- src/configs.ts
+++ src/configs.ts
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
+import { login_unif } from './connections/login_unificado';
 
 dotenv.config();
 
 export const { 
   PORT = 3000,
 } = process.env;
+
+
+export default async function testConnection() {
+  try {
+    await login_unif.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,28 @@
 import corsMiddleware from './corsConfig';
 
 import cookieParser from 'cookie-parser';
-import { PORT } from './configs';
+import testDB, { PORT } from './configs';
 import express from 'express';
 import morgan from 'morgan';
 
-
-import { login_unif } from './connections/login_unificado';
 import { userRouter } from './routes/user.routes';
 
 const v1 = '/api/v1';
 
-import 'dotenv/config';
-
 const app = express();
 
+// TODO: middlewares de express
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(corsMiddleware)
 
+// TODO: rutas de la API
 app.use(v1, userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const testConnection = async () => {
-  try {
-    await login_unif.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-
-testConnection();
+// TODO: test database connection
+testDB();
