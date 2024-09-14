01b1cbdbb27d0b6fdeaba4ee2b332a146345d8fb
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-14 09:28:17 -0500

    Add route to get personas

--- src/routes/persona.routes.ts
+++ src/routes/persona.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { getPersonas } from '../controllers/persona.controllers';
 
 export const personaRouter = Router();
 
+personaRouter.get('/personas', getPersonas);
