4c2c7785af5b28805fd2be2d1e9c174b258ebc74
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-15 13:23:10 -0500

    Refactor error handling in opciones.controllers.ts and add newArea route

--- src/controllers/opciones.controllers.ts
+++ src/controllers/opciones.controllers.ts
@@ -9,7 +9,7 @@ export const gellAllEmpresas = async (req: Request, res: Response) => {
     return res.status(200).json(empresas);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ msg: 'Internal server error' });
+    return res.status(500).json({ message: 'Internal server error' });
   }
 }
 
@@ -19,6 +19,35 @@ export const getAreas = async (req: Request, res: Response) => {
     return res.status(200).json(areas);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ msg: 'Internal server error' });
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+export const newArea = async (req: Request, res: Response) => {
+  const { codigo, nombre } = req.body;
+
+  if (!codigo || !nombre) {
+    return res.status(400).json({ message: 'código y nombre área son requeridos' });
+  }
+
+  try {
+    const exist = await Area.findOne({ where: { codigo } });
+
+    console.log(exist);
+
+    if (exist) {
+      return res.status(400).json({ message: 'El código de área ya existe' });
+    }
+
+    const result = await Area.create({ codigo, descripcion: nombre });
+
+    if (!result) {
+      return res.status(400).json({ message: 'No se pudo crear el área' });
+    }
+
+    return res.status(201).json({ message: 'Area Creada Correctamente' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 }
--- src/routes/opciones.routes.ts
+++ src/routes/opciones.routes.ts
@@ -1,7 +1,8 @@
-import { gellAllEmpresas, getAreas } from '../controllers/opciones.controllers';
+import { gellAllEmpresas, getAreas, newArea } from '../controllers/opciones.controllers';
 import { Router } from 'express';
 
 export const opcionesRouter = Router();
 
 opcionesRouter.get('/empresas', gellAllEmpresas);
 opcionesRouter.get('/areas', getAreas);
+opcionesRouter.post('/area', newArea);
