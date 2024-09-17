9763f6a415ff941f1190932487cb0c3eb6f7820c
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-17 08:37:36 -0500

    Refactor getMarcaciones function in marcacion.controllers.ts to include pagination functionality

--- src/controllers/marcacion.controllers.ts
+++ src/controllers/marcacion.controllers.ts
@@ -1,31 +1,40 @@
 import { Marcacion } from '../model/marcacion.model';
 import { Persona } from '../model/persona.model';
 import { Request, Response } from 'express';
-import { fn, col, Op } from 'sequelize';
 
 export const getMarcaciones = async (req: Request, res: Response) => {
-  try {
+  // Obtener los parámetros de paginación de la solicitud
+  const page = parseInt(req.query.page as string, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize as string, 10) || 20;
+  
+  // Calcular el offset
+  const offset = (page - 1) * pageSize;
 
-    const marcaciones = await Marcacion.findAll({
-      attributes: ['id', 'id_empleado', 'fecha_marcacion', 'estado_marcacion'], 
-      where: (fn('DATE', col('fecha_marcacion')), Op.eq, fn('CURDATE')),
-      // limit: 20,
+  try {
+    // Realizar la consulta con paginación
+    const { count, rows } = await Marcacion.findAndCountAll({
+      attributes: ['id', 'id_empleado', 'fecha_marcacion', 'estado_marcacion'],
+      limit: pageSize,
+      offset: offset,
+      order: [['id', 'DESC']],
       include: [{
         model: Persona,
-        attributes: ['nombres'], 
+        attributes: ['nombres'],
       }]
     });
 
-    const marcacionesFormateadas = marcaciones.map(marcacion => {
+    // Formatear los datos
+    const marcacionesFormateadas = rows.map(marcacion => {
       return {
         id: marcacion.id,
         nombres: marcacion.Persona.nombres,
-        fecha_marcacion: marcacion.fecha_marcacion.toDateString() + ' ' +  marcacion.fecha_marcacion.toTimeString().split(' ')[0],
+        fecha_marcacion: marcacion.fecha_marcacion.toDateString() + ' ' + marcacion.fecha_marcacion.toTimeString().split(' ')[0],
         estado_marcacion: marcacion.estado_marcacion
       }
-    })
+    });
 
-    return res.status(200).json(marcacionesFormateadas);
+    // Enviar la respuesta con los datos paginados
+    return res.status(200).json({ page, pageSize, total: count, marcaciones: marcacionesFormateadas });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal server error' });
