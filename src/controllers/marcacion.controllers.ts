49848a92b1b6cb2ef32109c77e478d7147bd6af5
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-27 09:18:51 -0500

    Refactor getMarcaciones function to include current date filter and order by Id

--- src/controllers/marcacion.controllers.ts
+++ src/controllers/marcacion.controllers.ts
@@ -36,7 +36,7 @@ export const getMarcaciones = async (req: Request, res: Response) => {
       return {
         id: m.Id,
         nombres: m.Persona!.nombres,
-        apellidos: m.Persona.apellidos,
+        apellidos: m.Persona!.apellidos,
         fecha: m.Fecha,
         hora: m.Hora,
         estado: m.estado
@@ -53,47 +53,42 @@ export const getMarcaciones = async (req: Request, res: Response) => {
 
 
 // TODO: organizar el audit marcacion
-// export const getAuditMarcacion = async (req: Request, res: Response) => {
-//   try {
-//     const result = await Marcacion.findAll({
-//       attributes: ['id', 'fecha_marcacion', 'estado_marcacion'],
-//       where: {
-//         [Op.and]: [
-//           where(fn('DATE', col('fecha_marcacion')), Op.eq, fn('CURDATE')),
-//           where(col('estado_marcacion'), Op.eq, 'Entrada')
-//         ],
-//       },
-//       include: [{
-//         attributes: ['nombres', 'apellidos'],
-//         model: Persona,
-//         as: 'Persona',
-//         where: { id_Grupo_Horario: { [Op.ne]: null } },
-//         include: [{
-//           attributes: ['diaSeman'],
-//           model: GrupoTurnoVsHorario,
-//           where: { diaSeman: getDayOfWeekString() },
-//           include: [{
-//             attributes: ['descripcion', 'hora_inicio'],
-//             model: Turnos
-//           }]
-//         }]
-//       }]
-//     });
+export const getAuditMarcacion = async (req: Request, res: Response) => {
+  try {
+    const result = await Marcacion.findAll({
+      attributes: ['Id', 'Hora', 'estado'],
+      where: { Fecha: { [Op.eq]: fn('CURDATE') } },
+      include: [{
+        attributes: ['nombres', 'apellidos'],
+        model: Persona,
+        as: 'Persona',
+        where: { id_Grupo_Horario: { [Op.ne]: null } },
+        include: [{
+          attributes: ['diaSeman'],
+          model: GrupoTurnoVsHorario,
+          where: { diaSeman: getDayOfWeekString() },
+          include: [{
+            attributes: ['descripcion', 'hora_inicio'],
+            model: Turnos
+          }]
+        }]
+      }]
+    });
 
-//     // const marcacionesFormateadas = result.map(marcacion => {
-//     //   return {
-//     //     id: result.id,
-//     //     nombres: result.Persona.nombres,
-//     //     apellidos: result.Persona.apellidos,
-//     //     hora_marcacion: result.fecha_result.toTimeString().split(' ')[0].slice(0, 5),
-//     //     estado_marcacion: result.estado_marcacion,
-//     //     hora_inicio: result.Persona.GrupoTurnoVsHorarios[0].Turno.hora_inicio
-//     //   }
-//     // })
+    const marcacionesFormateadas = result.map(marcacion => {
+      return {
+        id: marcacion.Id,
+        nombres: marcacion.Persona.nombres,
+        apellidos: marcacion.Persona.apellidos,
+        hora: marcacion.Hora.toString().slice(0, 5),
+        estado: marcacion.estado,
+        hora_inicio: marcacion.Persona.GrupoTurnoVsHorarios[0].Turno.hora_inicio
+      }
+    })
 
-//     return res.status(200).json({'result': result});
-//   } catch (error) {
-//     console.error('Error al obtener las marcaciones:', error);
-//     return res.status(500).json({ message: 'Internal server error' });
-//   }
-// };
+    return res.status(200).json(marcacionesFormateadas);
+  } catch (error) {
+    console.error('Error al obtener las marcaciones:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
