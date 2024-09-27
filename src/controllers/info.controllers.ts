2f79058afc6c57c4dade1dd2c6bd340804dd33c6
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-27 11:00:25 -0500

    Refactor infoMarcaciones function to include current date filter and order by Id

--- src/controllers/info.controllers.ts
+++ src/controllers/info.controllers.ts
@@ -1,15 +1,33 @@
-import { fn, Op } from 'sequelize';
 import { Marcacion } from '../model/marcacion.model'
 import { Request, Response } from 'express';
+import { Estado } from '../types/info';
+import { fn, Op } from 'sequelize';
 
+const estadosMap: { [key: string]: Estado } = {
+  'Entrada': Estado.Entrada,
+  'Salida_intermedia': Estado.SalidaIntermedia,
+  'Entrada_intermedia': Estado.EntradaIntermedia,
+  'Salida': Estado.Salida
+};
 export async function infoMarcaciones(req: Request, res: Response) {
 
   try {
-    const result = await Marcacion.findAll({
+    const { rows, count } = await Marcacion.findAndCountAll({
       where: { Fecha: { [Op.eq]: fn('CURDATE') } }
     })
 
-    return res.status(200).json(result)
+    const stados = rows.reduce((acc, mar) => {
+      const key = estadosMap[mar.estado];
+      if (key) { acc[key] += 1; }
+      return acc
+    }, { 
+      [Estado.Entrada]: 0,
+      [Estado.SalidaIntermedia]: 0,
+      [Estado.EntradaIntermedia]: 0,
+      [Estado.Salida]: 0
+    })
+
+    return res.status(200).json({ count, stados })
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal server error' })
